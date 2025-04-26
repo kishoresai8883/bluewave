0db319823e20c7f66904c41c46f9324721295627
Kishore Sai <kishore.sai207@gmail.com>
2025-04-26 09:54:49 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // allows access via LAN IP
-    port: 5225,      // optional, default is 5173
-  },
+  base: '/bluewave/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
